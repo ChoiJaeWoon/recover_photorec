13c0e88775f6f8a86486f7ba325e7b34bdb100bc
ChoiJaeWoon <152960408+ChoiJaeWoon@users.noreply.github.com>
2024-05-27 20:00:35 +0900

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ const port = process.env.PORT || 3000;
 
 const upload = multer({ storage: multer.memoryStorage() });
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/file_recovery')
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => {
     console.log("Connected to MongoDB.");
   })
