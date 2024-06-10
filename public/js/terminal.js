c707ad9c35af593d473145afa4da0719b58c6c21
ChoiJaeWoon <152960408+ChoiJaeWoon@users.noreply.github.com>
2024-06-10 17:47:24 +0900

    Add files via upload

---
+++ public/js/terminal.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const socket = io();
+  
+    const terminalContainer = document.getElementById("terminal-container");
+    const xterm = new Terminal();
+    xterm.open(terminalContainer);
+  
+    socket.on("output", (data) => {
+      xterm.write(data);
+    });
+  
+    xterm.onKey(e => {
+      const input = e.key;
+      socket.emit("input", input);
+    });
+  
+    socket.on("photorec-output", (data) => {
+      xterm.write(data);
+    });
+  
+    window.addEventListener("resize", () => {
+      const cols = xterm.cols;
+      const rows = xterm.rows;
+      socket.emit("resize", { cols, rows });
+    });
+  });
+  
